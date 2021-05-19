ec8d455f7f8744f47f606b8b54e4477f2eb35662
tmxkqotnl <tmxkqotnl@gmail.com>
2021-05-19 17:16:57 +0900

    nodejs setting

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const {User} = require('./models/User');
+const {MONGO_URI} = require('./config/Key');
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://Admin:${process.env.MONGO_PASSWORD}@mongodbtutorial.5lxlo.mongodb.net/react`,{
+mongoose.connect(MONGO_URI,{
   useNewUrlParser:true,
   useCreateIndex:true,
   useUnifiedTopology:true,
   useFindAndModify:false,
-},()=>{
-  console.log("DB Connected...");
+},(err)=>{
+  if(!err){
+    console.log("DB Connected...");
+  }else{
+    console.error(err);
+  }
 });
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(process.env.NODE_ENV === "production" ? morgan('combined'):morgan('dev'));
 
 app.set('port',process.env.PORT||5000);
+
+app.get('/',(req,res)=>{
+  res.send('hello');
+});
+
+app.post('/register',(req,res)=>{
+  // 회원가입 정보를 DB에 저장한다.
+  const newUser = new User(
+    req.body
+  );
+  newUser.save((err,result)=>{
+    if(err) return res.json({success:false, err});
+    return res.status(200).json({
+      success:true
+    });
+  });
+});
 
 app.listen(app.get('port'),()=>{
   console.log('listening on port ',app.get('port'));
--- models/User.js
+++ models/User.js
@@ -1,32 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRound = 15;
+
 // 몽구스 스키마
-const Schema = mongoose.Schema({
-  name:{
+const userSchema = mongoose.Schema({
+  name: {
     type: String,
-    maxlength:50,
+    maxlength: 50,
+    require: true,
   },
-  email:{
-    type:String,
-    trim:true, // 공백 제거
-    maxlength:60,
-    unique:true,
-  },
-  lastname:{
+  email: {
     type: String,
-    maxlength:50,
+    trim: true, // 공백 제거
+    maxlength: 60,
+    unique: true,
+    require: true,
   },
-  role:{
-    type:Number,
-    default:0,
+  password: {
+    type: String,
+    require: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
   },
   image: String,
-  token:{
-    type:String,
+  token: {
+    type: String,
   },
-  tokenExp:{
-    type:Number,
-  }
-})
+  tokenExp: {
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User',Schema); // 모델로 감싸준다.
-module.exports = {Schema}; // 외부에서 사용하도록 export
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (user.isModified("password") || user.isNew) {
+    bcrypt.genSalt(saltRound, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+const User = mongoose.model("User", userSchema); // 모델로 감싸준다.
+module.exports = { User }; // 외부에서 사용하도록 export
